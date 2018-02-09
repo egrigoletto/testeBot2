e97a121e78c3c5464c24ec36832b1cc36be6d804
egrigoletto <36228773+egrigoletto@users.noreply.github.com>
2018-02-09 13:21:09 -0200

    inserção de listeners

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ var express = require('express');
 
 var router = express();
 
+app.set('port', (process.env.PORT || 3000))
+app.use(express.static(__dirname + '/public'))
+
 //Caminho para receber get 
 router.get('/webhook', function(req, res){
     res.send('ok')
 });
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
